aba0a59a6b255ab4b9b139fe7b3c4ddd6869ac76
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-09-28 20:14:08 -0300

    teste para retorno falso de nao existencia de email no banco de dados do sistema + active profile para test com banco h2

--- src/test/java/br/com/tiagocordeiro/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/tiagocordeiro/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
     @Autowired
@@ -24,6 +26,19 @@ public class UsuarioRepositoryTest {
         boolean resultado = usuarioRepository.existsByEmail("tiago@gmail.com");
         // verificação
         Assertions.assertThat(resultado).isTrue();
+    }
+
+    @Test
+    public void deveRetornarFalsoQuandoVerificarNaoExistenciaDeEmail() {
+        // garantir que a base esteja limpa
+        // cenario
+        usuarioRepository.deleteAll();
+
+        // ação
+        boolean resultado = usuarioRepository.existsByEmail("emailqualquer@gmail.com");
+
+        // verificação
+        Assertions.assertThat(resultado).isFalse();
     }
 
 }
